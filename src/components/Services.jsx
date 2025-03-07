e60619b261ba069ccd57513a1e0453952cd84029
ArinSunam <arinsunam05@gmail.com>
2025-03-07 14:21:26 +0545

    responsive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -23,9 +23,9 @@ const Services = () => {
 
   return (
     <div className='bg-[#f5f5f5] py-[80px]'>
-      <div className='flex justify-between mycontainer '>
-        {serviceData.map((el) => (
-          <div className='flex items-center'>
+      <div className='flex flex-col sm:flex-row sm:flex-wrap items-center justify-between mycontainer gap-y-7 gap-x-4 '>
+        {serviceData.map((el, i) => (
+          <div className={`flex items-center ${i === 2 && "pr-12"}`}>
             <div className='text-[#F28123] mr-[15px] border-2 border-[#F28123] border-dotted rounded-full size-[65px] flex items-center justify-center' >{el.icon}</div>
             <div className='text-[#051922] space-y-1'>
               <h3 className='text-lg font-bold '>{el.title}</h3>
