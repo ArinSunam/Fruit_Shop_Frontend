85d7200641916ea6989f0690d29b86e6033f35a2
ArinSunam <arinsunam05@gmail.com>
2025-03-30 09:18:17 +0545

    cart feature

--- src/features/Storage.js
+++ src/features/Storage.js
@@ -10,3 +10,16 @@ export const getUser = () => {
 export const clearAllData = () => {
   localStorage.clear()
 }
+
+export const setCartsToLocal = (carts) => {
+  localStorage.setItem('carts', JSON.stringify(carts));
+}
+
+export const getCartsFromLocal = () => {
+  const carts = localStorage.getItem('carts');
+  return carts === null ? [] : JSON.parse(carts);
+}
+
+export const clearCartsFromLocal = () => {
+  localStorage.getItem('carts');
+}
--- src/features/store.js
+++ src/features/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './UserSlice'
 import { ProductApi } from "./ProductApi";
 import { AuthApi } from "./AuthApi";
+import cartReducer from './cartSlice'
 
 
 export const store = configureStore({
   reducer: {
     userInfo: userReducer,
+    cartData: cartReducer,
     [ProductApi.reducerPath]: ProductApi.reducer,
     [AuthApi.reducerPath]: AuthApi.reducer
   },
